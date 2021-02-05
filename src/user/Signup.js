7d69b1618241635b71b341219639116eaa59a8f5
Claire <claire.cy.t@gmail.com>
2021-02-05 15:11:38 -0500

    refactor signup to auth folder

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 
-import { API } from '../config'
 import Layout from '../core/Layout'
 import {Link} from 'react-router-dom'
+import {signup} from '../auth'
 
 const Signup = () => {
   const [values, setValues] = useState({
@@ -19,24 +19,6 @@ const Signup = () => {
     setValues({ ...values, error: false, [name]: event.target.value })
   }
 
-  const signup = (user) => {
-    console.log(name, email, password)
-    return fetch(`${API}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: 'application/json',
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
-    })
-      .then(response => {
-        return response.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
-
-  }
   const clickSubmit = (event) => {
     event.preventDefault()
     setValues({...values, error: false})
